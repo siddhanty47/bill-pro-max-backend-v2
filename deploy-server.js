deb94854ef55008cb9bb71b2958395f34403cc3e
siddhanty47 <44899000+siddhanty47@users.noreply.github.com>
2026-03-13 14:39:19 +0530

    git auto-deploy for windows

--- deploy-server.js
+++ deploy-server.js
@@ -1,27 +1,5 @@
-const express = require("express")
 const { exec } = require("child_process")
 
-const app = express()
-
-app.use(express.json())
-
-app.post("/deploy", (req, res) => {
-
-  console.log("Webhook received")
-
-  exec("./deploy.sh", (err, stdout, stderr) => {
-
-    if (err) {
-      console.error(err)
-      return
-    }
-
-    console.log(stdout)
-  })
-
-  res.send("Deployment started")
-})
-
-app.listen(9000, () => {
-  console.log("Webhook server running on port 9000")
+exec("deploy.bat", (err, stdout, stderr) => {
+  console.log(stdout)
 })
